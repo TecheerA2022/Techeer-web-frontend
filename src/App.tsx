c5c46035493a2f0a9fc0a7860ddac163433cba00
Yeonju Seo(서연주) <44168693+h01010@users.noreply.github.com>
2022-04-30 01:16:48 -0700

    (TAM-main) Typescript화

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import ProjectTile from "../../components/ProjectTile";
+
+export default function Home() {
+  const projectDatas = [
+    {
+      id: "0001",
+      projectTitle: "projectA",
+      projectTeam: "Team A",
+      projectDesc:
+        "멘토링 프로그램 테커 사이트를 만드는 프로젝트이며 테커 사이트를 만드는 프로젝트이며 프로젝트이다.",
+      projectImg:
+        "https://upload.wikimedia.org/wikipedia/commons/e/ee/Sample_abc.jpg",
+    },
+    {
+      id: "0002",
+      projectTitle: "projectB",
+      projectTeam: "Team B",
+      projectDesc:
+        "멘토링 프로그램 테커 사이트를 만드는 프로젝트이며 테커 사이트를 만드는 프로젝트이며 프로젝트이다.",
+      projectImg:
+        "https://upload.wikimedia.org/wikipedia/commons/1/16/HDRI_Sample_Scene_Balls_%28JPEG-HDR%29.jpg",
+    },
+    {
+      id: "0003",
+      projectTitle: "projectC",
+      projectTeam: "Team C",
+      projectDesc:
+        "멘토링 프로그램 테커 사이트를 만드는 프로젝트이며 테커 사이트를 만드는 프로젝트이며 프로젝트이다.",
+      projectImg:
+        "https://images.unsplash.com/photo-1579353977828-2a4eab540b9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2FtcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+    },
+  ];
+
+  return (
+    <Grid
+      container
+      spacing={2}
+      justifyContent="center"
+      alignItems="center"
+      style={{ marginTop: "auto" }}
+    >
+      {/* {projectDatas.map((item) => {
+        <Grid item xs={4}>
+          <ProjectTile
+            id={item.id}
+            title={item.projectTitle}
+            team={item.projectTeam}
+            img={item.projectImg}
+            desc={item.projectDesc}
+          />
+        </Grid>;
+      })} */}
+      <Grid item xs={4}>
+        <ProjectTile />
+      </Grid>
+      <Grid item xs={4}>
+        <ProjectTile />
+      </Grid>
+      <Grid item xs={4}>
+        <ProjectTile />
+      </Grid>
+    </Grid>
+  );
+}
