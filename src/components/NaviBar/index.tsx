684df4abc43d4ab5372b7d6f0e543fe671f7e415
Yeonju Seo(서연주) <44168693+h01010@users.noreply.github.com>
2022-06-11 21:08:04 -0700

    (TAM-27) 네비바 스타일 업데이트

--- src/components/NaviBar/index.tsx
+++ src/components/NaviBar/index.tsx
@@ -1,17 +1,31 @@
 import * as React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 import "./style.css";
+import Techeer_Logo_Basic from "../../assets/images/Techeer_Logo_Basic.png";
+import Techeer_Logo_Circle from "../../assets/images/Techeer_Logo_circle.png";
 
 const Navibar = () => {
   return (
     <Navbar className="navbar-body" collapseOnSelect expand="lg" variant="dark">
-      <Navbar.Brand href="/">Techeer;</Navbar.Brand>
+      {/* <Navbar.Brand href="/">
+        Techeer;
+      </Navbar.Brand> */}
+      <Navbar.Brand href="/">
+        <img
+          src={Techeer_Logo_Circle}
+          height="40"
+          className="d-inline-block align-top navbar-logo"
+          alt="Techeer Logo"
+        />{" "}
+        <span className="navbar-title">Techeer</span>
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto">
+        <Nav className="me-auto navbar-link">
           <Nav.Link href="/project">Projects</Nav.Link>
           <Nav.Link href="/member">Members</Nav.Link>
-          <Nav.Link href="/board">Board</Nav.Link>
+          {/* 보류 기능 */}
+          {/* <Nav.Link href="/board">Board</Nav.Link> */}
         </Nav>
         <Nav>
           <NavDropdown title="Profile" id="collasible-nav-dropdown">
